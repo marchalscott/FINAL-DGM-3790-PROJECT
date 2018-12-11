fbd0acf31d07d88121aa5a8999d8f8ef55ea1217
Marchal <marchalscott@gmail.com>
2018-12-10 20:10:18 -0700

    Final Project for DGM 3790.

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h1>Trade or view your Crypto Portfolio</h1>
+		<h3>How the site works.</h3>
+		<h6>- We display your total wallet amount in the top right corner.</h6>
+		<h6>- Please use your wallet balance to purchase your favorite crypto currency.</h6>
+		<h6>- Watch the date, buy on low and sell on high. Grow your profits!</h6>
+		<h6>- Click on End Day to begin a new day</h6>
+		<hr>
+
+<p>Your Funds: {{ funds | currency }}</p>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		computed: {
+			funds() {
+				return this.$store.getters.funds;
+			}
+		}
+	}
+</script>
---
+++ src/data/stocks.js
@@ -0,0 +1,12 @@
+export default [
+	{id: 1,  name: 'Bitcoin', price: 3000},
+	{id: 2,  name: 'Bitcoin Cash', price: 100},
+	{id: 3,  name: 'Ethereum', price: 600},
+	{id: 4,  name: 'NEO', price: 300},
+	{id: 5,  name: 'Ripple', price: 100},
+	{id: 6,  name: 'Litecoin', price: 90},
+	{id: 7,  name: 'Dash', price: 70},
+	{id: 8,  name: 'NeoGas', price: 20},
+	{id: 9,  name: 'BRD', price: 30},
+	{id: 10, name: 'Waves', price: 5}
+];
